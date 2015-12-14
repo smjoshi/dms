69ad752830ad066685eff1ec1e4783bb7e969207
Sachin Joshi <mailtosmj@gmail.com>
2015-12-13 22:51:48 -0500

    development changes

---
+++ dms-web/src/it/java/com/dc/dms/web/controller/LoginControllerIT.java
@@ -0,0 +1,19 @@
+package com.dc.dms.web.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginControllerIT {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testRegisterUserITCase() {
+		fail("Not yet implemented");
+	}
+
+}
