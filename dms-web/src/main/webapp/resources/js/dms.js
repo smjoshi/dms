57e01e91fa339fd02e3727955bbacc1f51320976
unknown <SJoshi@MSC-SJOSHILP.man.co>
2016-02-15 21:13:53 -0500

    reactjs updates

---
+++ dms-web/src/main/webapp/resources/js/dms.js
@@ -0,0 +1,21 @@
+/**
+ * React.js code for dms page
+ */
+
+var ImgContainer = React.createClass({
+	render: function(){
+	// rendering DOM updates
+	return 	<h1> Images will be displayed Here </h1>;
+				
+	}
+	
+});
+
+
+ReactDOM.render(
+	<ImgContainer productId="123"/>,
+	document.getElementById('dmscontainer')
+);
+	
+
+
