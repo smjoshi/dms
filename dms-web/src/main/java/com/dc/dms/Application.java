cb54c97eaaca82f129b459cdaaf36bb4c388f072
sjoshi <mailtosmj@gmail.com>
2016-08-23 15:42:58 +0530

    Spring boot refactoring

--- dms-web/src/main/java/com/dc/dms/Application.java
+++ dms-web/src/main/java/com/dc/dms/Application.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +19,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, WebMvcAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
 @ComponentScan
 public class Application {
 	
@@ -39,30 +41,5 @@ public class Application {
 	}
 	
 	
-	@Bean
-	public DispatcherServlet dispatcherServlet(){
-		return new DispatcherServlet();
-	}
 
-	
-	@Bean
-	public ServletRegistrationBean dispatcherServletRegistration(){
-		ServletRegistrationBean dispatcherRegistration = new ServletRegistrationBean(this.dispatcherServlet(), "/web/*");
-		dispatcherRegistration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
-		
-		return dispatcherRegistration;
-		
-	}
-	
-	
-//	/**
-//	 * @return
-//	 */
-//	@Bean 
-//	ServletRegistrationBean jerseyServletRegistration(){
-//		ServletRegistrationBean jerSeyRegistration = new ServletRegistrationBean(new ServletContainer(), "/api/*");
-//		jerSeyRegistration.addInitParameter(ServletProperties.JAXRS_APPLICATION_CLASS, RestApplicationConfig.class.getName());
-//		
-//		return jerSeyRegistration;
-//	}
 }
--- dms-web/src/main/java/com/dc/dms/config/WebConfig.java
+++ dms-web/src/main/java/com/dc/dms/config/WebConfig.java
@@ -1,18 +1,20 @@
 package com.dc.dms.config;
 
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = { "com.dc.dms.web.controller" })
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcAutoConfigurationAdapter {
 
 	@Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
@@ -28,5 +30,34 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
+	
+//	@Bean (name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
+//	public DispatcherServlet dispatcherServlet(){
+//		return new DispatcherServlet();
+//	}
+//
+//	
+//	@Bean
+//	public ServletRegistrationBean dispatcherServletRegistration(){
+//		ServletRegistrationBean dispatcherRegistration = new ServletRegistrationBean(this.dispatcherServlet(), "/web/*");
+//		dispatcherRegistration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
+//		
+//		return dispatcherRegistration;
+//		
+//	}
+	
+	
+//	/**
+//	 * @return
+//	 */
+//	@Bean 
+//	ServletRegistrationBean jerseyServletRegistration(){
+//		ServletRegistrationBean jerSeyRegistration = new ServletRegistrationBean(new ServletContainer(), "/api/*");
+//		jerSeyRegistration.addInitParameter(ServletProperties.JAXRS_APPLICATION_CLASS, RestApplicationConfig.class.getName());
+//		
+//		return jerSeyRegistration;
+//	}
+	
+	
 
 }
