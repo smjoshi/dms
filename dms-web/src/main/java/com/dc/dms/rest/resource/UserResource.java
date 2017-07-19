92c4b56f9f8166ca9f598c664aa5d3b7dac028f9
Sachin Joshi <mailtosmj@gmail.com>
2017-07-19 08:55:08 -0400

    flyway changes

--- dms-web/src/main/java/com/dc/dms/rest/resource/UserResource.java
+++ dms-web/src/main/java/com/dc/dms/rest/resource/UserResource.java
@@ -69,10 +69,10 @@ public class UserResource {
 
 		try {
 				createdUser = userService.registerUser(user);
-		} catch (DuplicateUserException | DMSException e ) {
-			e.printStackTrace();
+		} catch ( Throwable t) {
+			t.printStackTrace();
 			createdUser = null;
-			logger.debug("{exception:"+ e.getCause() + "}");
+			logger.debug("{exception:"+ t.getCause() + "}");
 			throw new ApplicationRestException();
 		}
 
